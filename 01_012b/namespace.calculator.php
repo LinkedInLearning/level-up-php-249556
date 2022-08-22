8e02c7771535813117be1da44ec4b128bd060f2f
Joe Casabona <jcasabona@gmail.com>
2022-08-22 15:59:26 -0400

    Update namespace.calculator.php

--- 01_012b/namespace.calculator.php
+++ 01_012b/namespace.calculator.php
@@ -1,3 +1,25 @@
 <?php
 
-// Define your namespace here!
+function add($numbers)
+{
+}
+
+function subtract($start, $numbers)
+{
+}
+
+function multiply($numbers)
+{
+}
+
+function divide($start, $numbers)
+{
+}
+
+function remainder($start, $numbers)
+{
+}
+
+function average($numbers)
+{
+}
