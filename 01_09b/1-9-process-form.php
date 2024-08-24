dd9f7d67a4e333a70a2bef21b564cc7d55678f0d
Joe Casabona <jcasabona@gmail.com>
2024-08-24 06:05:32 -0400

    update form

--- 01_09b/1-9-process-form.php
+++ 01_09b/1-9-process-form.php
@@ -24,8 +24,33 @@ function process_form() {
 					<input type="email" name="email" placeholder="What's Your Email?" />
 				</div>
 				<div>
+					<label for="type_of_message">Type of Message:</label>
+					<select name="type_of_message" id="type_of_message">
+						<option value="Question">Question</option>
+						<option value="Comment">Comment</option>
+						<option value="Concern">Concern</option>
+						<option value="Something Else">Something Else</option>
+					</select>
+				</div>
+				<div>
 					<label for="message">Your Message:</label>
 					<textarea name="message"></textarea>
+				</div>
+				<div>
+					<label for="heard_about">How did you hear about us?</label><br>
+					<input type="radio" id="podcast" name="heard_about" value="podcast">
+					<label for="podcast">Podcast</label><br>
+					<input type="radio" id="newsletter" name="heard_about" value="newsletter">
+					<label for="newsletter">Newsletter</label><br>
+					<input type="radio" id="youtube" name="heard_about" value="youtube">
+					<label for="youtube">YouTube</label><br>
+					<input type="radio" id="social_media" name="heard_about" value="social_media">
+					<label for="social_media">Social Media</label><br>
+					<input type="radio" id="something_else" name="heard_about" value="something_else">
+					<label for="something_else">Something Else</label><br>
+				</div>
+				<div>
+					<input type="checkbox" value="true" name="join_mailing_list"><label for="join_mailing_list">Join the Mailing List?</label>
 				</div>
 				<div><input type="submit" name="submit" value="Send Message" /></div>
 			</form>	
