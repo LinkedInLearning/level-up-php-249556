0530dda0a53a76d006dcad5928824738eb7cda06
Joe Casabona <jcasabona@gmail.com>
2024-09-04 09:31:49 -0400

    Refine 1-23

--- 01_023b/1-23-count.php
+++ 01_023b/1-23-count.php
@@ -2,4 +2,49 @@
   function count_words( $file ) {
     //write this function
   }
+
+	$words = count_words( 'transcript.txt' );
 ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Word Counter</title>
+        <meta name="author" value="Joe Casabona" />
+				<style>
+					table {
+						margin: 25px auto;
+						max-width: 800px;
+						border: 1;
+						font-size: 1.5em;
+					}
+					table, th, td {
+						border: 1px solid black;
+						border-collapse: collapse;
+					}
+
+					th, td {
+						width: 50%;
+						padding: 7px;
+					}
+				</style>
+    </head>
+    <body>
+        <table>
+					<thead>
+						<th>Word</th>
+						<th>Count</th>
+					</thead>
+					<tbody>
+						<?php
+							$format = '<tr><td>%1$s</td><td>%2$s</td></tr>';
+							foreach( $words as $word => $count ) {
+								printf( $format, 
+									$word, 
+									$count
+								);
+							}
+						?>
+					</tbody>
+				</table>
+    </body>
+</html>
