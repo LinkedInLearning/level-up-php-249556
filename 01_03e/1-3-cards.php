af392370983a41b6d24ad8f4f73345a0359b875c
Joe Casabona <jcasabona@gmail.com>
2024-09-09 14:12:37 -0400

    Code for video

--- 01_03e/1-3-cards.php
+++ 01_03e/1-3-cards.php
@@ -3,26 +3,25 @@
 	$values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A' ];
 	$suits = ['C', 'D', 'H', 'S']; // Club, Diamond, Heart, Spade
 
-	$p1_value_index = rand(0, sizeof($values) - 1);
-	$p2_value_index = rand(0, sizeof($values) - 1); 
-	$p1_suit_index = rand(0, sizeof($suits) - 1);
-	$p2_suit_index = rand(0, sizeof($suits) - 1); 
+	$p1_value_index = rand(0, sizeof($values) - 1 );
+	$p2_value_index = rand(0, sizeof($values) - 1 );
+	$p1_suit_index = rand(0, sizeof($suits) - 1 );
+	$p2_suit_index = rand(0, sizeof($suits) - 1 );
 
-	$player_one = $values[$p1_value_index] . $suits[$p1_suit_index];
-	$player_two = $values[$p2_value_index] . $suits[$p2_suit_index];
+	$p1_card = $values[$p1_value_index] . $suits[$p1_suit_index];
+	$p2_card = $values[$p2_value_index] . $suits[$p2_suit_index];
 
-	$cards = "Player One has $player_one. Player Two has $player_two. ";
+	$cards = "<p>Player One has $p1_card. Player Two has $p2_card. ";
 
-	if( $p1_value_index > $p2_value_index ) { // Compare the index value, which directly associate with the card's value
+	if( $p1_value_index > $p2_value_index ) { 
 		$winner = "Player One wins!";
-	} else if ( $p2_value_index > $p1_value_index ) {
-		$winner = "Player Two wins!";
+	} else if( $p2_value_index > $p1_value_index ) {
+		$winner = "Player Two wins!"; 
 	} else {
 		$winner = "It's a draw!";
 	}
 
-	echo $cards . $winner;
-
+	echo $cards . $winner . "</p>";
 ?>
 
 <p><a href="<?php echo $_SERVER['PHP_SELF']; ?>">Play again?</a></p>
